a65bc30e7a29480c6da61be2e963844f3b216e8f
Eugene <evgkisevgkis@gmail.com>
2024-08-13 12:13:01 +0500

    создание модели товара

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование'),
+    description = models.CharField(max_length=150, verbose_name='описание'),
+    image = models.ImageField(upload_to='images', blank=True, null=True, verbose_name='изображение')
+    category = models.CharField(max_length=50, verbose_name='категория'),
+    price = models.DecimalField(max_length=15, verbose_name='цена за штуку'),
+    created = models.DateField(verbose_name='дата создания'),
+    changed = models.DateField(verbose_name='дата последнего изменения')
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
