565ab45be83f4d2a6757cc7775432ccbdca7c6fd
Eugene <evgkisevgkis@gmail.com>
2024-08-27 13:35:49 +0500

    изменение моделей

--- catalog/models.py
+++ catalog/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     image = models.ImageField('изображение', upload_to='images', blank=True, null=True)
     category = models.CharField('категория', max_length=50)
     price = models.DecimalField('цена за штуку', max_digits=10, decimal_places=2)
-    created = models.DateField('дата создания')
+    created = models.DateField('дата создания', default=django.utils.timezone.now)
     changed = models.DateField('дата последнего изменения')
 
     class Meta:
@@ -20,7 +20,7 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField('наименование', max_length=50)
     description = models.CharField('описание', max_length=150)
-    created_at = models.DateField('дата создания', default=django.utils.timezone.now())
+    created_at = models.DateField('дата создания', default=django.utils.timezone.now)
 
     class Meta:
         verbose_name = 'категория'
