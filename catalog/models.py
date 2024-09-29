d03991954596db4a0a44f1fae56b2820d72da787
Eugene <evgkisevgkis@gmail.com>
2024-09-29 21:21:42 +0500

    создание кастомной команды для очистки и наполнения БД

--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,17 @@
 import django.utils.timezone
-from django.db import models
+from django.db import models, connection
 
 
 class Category(models.Model):
     name = models.CharField('наименование', max_length=50)
     description = models.CharField('описание', max_length=150)
     created_at = models.DateField('дата создания', default=django.utils.timezone.now)
+
+    @classmethod
+    def truncate(cls):
+        with connection.cursor() as cursor:
+            cursor.execute('TRUNCATE TABLE catalog_category CASCADE')
+            cursor.execute('ALTER SEQUENCE catalog_category_id_seq RESTART WITH 1')
 
     class Meta:
         verbose_name = 'категория'
@@ -18,6 +24,12 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     price = models.DecimalField('цена за штуку', max_digits=10, decimal_places=2)
     created = models.DateField('дата создания', default=django.utils.timezone.now)
+
+    @classmethod
+    def truncate(cls):
+        with connection.cursor() as cursor:
+            cursor.execute('TRUNCATE TABLE catalog_product CASCADE')
+            cursor.execute('ALTER SEQUENCE catalog_product_id_seq RESTART WITH 1')
 
     class Meta:
         verbose_name = 'товар'
