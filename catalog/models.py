12b6198120d0522a55739efd04594b315edd1435
Eugene <evgkisevgkis@gmail.com>
2024-09-29 16:50:21 +0500

    создание связи в модели Категории

--- catalog/models.py
+++ catalog/models.py
@@ -1,20 +1,5 @@
 import django.utils.timezone
 from django.db import models
-import datetime
-
-
-class Product(models.Model):
-    name = models.CharField('наименование', max_length=50)
-    description = models.CharField('описание', max_length=150)
-    image = models.ImageField('изображение', upload_to='images', blank=True, null=True)
-    category = models.CharField('категория', max_length=50)
-    price = models.DecimalField('цена за штуку', max_digits=10, decimal_places=2)
-    created = models.DateField('дата создания', default=django.utils.timezone.now)
-    changed = models.DateField('дата последнего изменения')
-
-    class Meta:
-        verbose_name = 'товар'
-        verbose_name_plural = 'товары'
 
 
 class Category(models.Model):
@@ -25,3 +10,16 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+class Product(models.Model):
+    name = models.CharField('наименование', max_length=50)
+    description = models.CharField('описание', max_length=150)
+    image = models.ImageField('изображение', upload_to='images', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.DecimalField('цена за штуку', max_digits=10, decimal_places=2)
+    created = models.DateField('дата создания', default=django.utils.timezone.now)
+    changed = models.DateField('дата последнего изменения')
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
