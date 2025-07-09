8407398d350af1f17a81aad0a1815e24d8bc631f
Eugene <evgkisevgkis@gmail.com>
2025-07-09 21:38:45 +0500

    создание CRUD для статей блога

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from blog.models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+    ordering = '-date_created'
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ('name', 'content', 'image', 'is_published')
+    success_url = reverse_lazy('blog:blog')
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('__all__')
+    def get_success_url(self):
+        return reverse_lazy('blog:blog-detail', kwargs={'pk': self.object.pk})
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = reverse_lazy('blog:blog')
+
--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls', 'blog')),
 ]
 
 if settings.DEBUG:
