d7335b0bb241b4e5d2b3792331820871d87fed0a
Eugene <evgkisevgkis@gmail.com>
2025-07-17 16:46:34 +0500

    добавление счетчика просмотров

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,12 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class ArticleCreateView(CreateView):
     model = Article
