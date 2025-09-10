8d127a792d9b5f316b1e32e8be0dd55b2a2edfa8
Eugene <evgkisevgkis@gmail.com>
2025-09-10 21:40:41 +0500

    добавление валидации названия и описания в форме товара

--- catalog/forms.py
+++ catalog/forms.py
@@ -12,3 +12,20 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ('created', )
+
+    bad_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        for bad_word in self.bad_words:
+            if bad_word in cleaned_data:
+                raise forms.ValidationError('В наименовании товара не должно быть запрещенных слов')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        for bad_word in self.bad_words:
+            if bad_word in cleaned_data:
+                raise forms.ValidationError('В описании товара не должно быть запрещенных слов')
+        return cleaned_data
+
