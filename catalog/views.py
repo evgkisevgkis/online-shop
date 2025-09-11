096877e33095355eea3bc5f724062654ff3f87e7
Eugene <evgkisevgkis@gmail.com>
2025-09-11 22:04:40 +0500

    добавление формсета для работы с товарами и версиями

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
+from django.db import transaction
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from catalog.forms import ContactForm, ProductForm
-from catalog.models import Product
+from catalog.forms import ContactForm, ProductForm, VersionForm
+from catalog.models import Product, Version
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
@@ -28,11 +30,55 @@ class ProductCreateView(CreateView):
     form_class = ProductForm
     success_url = reverse_lazy('home')
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        FormSet = inlineformset_factory(self.model, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = FormSet(self.request.POST, instance=self.object)
+        else:
+            formset = FormSet(instance=self.object)
+        context_data['formset'] = formset
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        with transaction.atomic():
+            if form.is_valid():
+                self.object = form.save()
+                if formset.is_valid():
+                    formset.instance = self.object
+                    formset.save()
+
+        return super().form_valid(form)
+
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     def get_success_url(self):
         return reverse_lazy('item', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        FormSet = inlineformset_factory(self.model, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = FormSet(self.request.POST, instance=self.object)
+        else:
+            formset = FormSet(instance=self.object)
+        context_data['formset'] = formset
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        with transaction.atomic():
+            if form.is_valid():
+                self.object = form.save()
+                if formset.is_valid():
+                    formset.instance = self.object
+                    formset.save()
+
+        return super().form_valid(form)
 
 
 class ProductDeleteView(DeleteView):
