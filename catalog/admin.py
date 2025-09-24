e8b0c8e93e42b41ebc4484c49c4e69bfc6fd55fe
Eugene <evgkisevgkis@gmail.com>
2025-09-24 21:55:28 +0500

    добавление версии в админ панель

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Version
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('pk' ,'name',)
+    list_display = ('pk' ,'name')
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'price', 'category')
     list_filter = ('category',)
-    search_fields = ('name', 'description',)
+    search_fields = ('name', 'description')
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product', 'number', 'flag')
+    list_filter = ('product',)
 
