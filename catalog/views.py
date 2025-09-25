eafb518bc431d1f46752b9578152732f05861768
Eugene <evgkisevgkis@gmail.com>
2025-09-25 21:32:54 +0500

    редактирование шаблона и create view

--- catalog/views.py
+++ catalog/views.py
@@ -30,27 +30,6 @@ class ProductCreateView(CreateView):
     form_class = ProductForm
     success_url = reverse_lazy('home')
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        FormSet = inlineformset_factory(self.model, Version, form=VersionForm, extra=1)
-        if self.request.method == 'POST':
-            formset = FormSet(self.request.POST, instance=self.object)
-        else:
-            formset = FormSet(instance=self.object)
-        context_data['formset'] = formset
-        return context_data
-
-    def form_valid(self, form):
-        context_data = self.get_context_data()
-        formset = context_data['formset']
-        with transaction.atomic():
-            if form.is_valid():
-                self.object = form.save()
-                if formset.is_valid():
-                    formset.instance = self.object
-                    formset.save()
-
-        return super().form_valid(form)
 
 class ProductUpdateView(UpdateView):
     model = Product
