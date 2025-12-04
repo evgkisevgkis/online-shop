a0f1067bd908f826dd36127817e10dda3cb2a87d
Eugene <evgkisevgkis@gmail.com>
2025-12-04 20:55:12 +0500

    создание login и logout

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +152,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.views import LoginView, LogoutView
+
 from users.apps import UsersConfig
 from django.urls import path
 from users.views import RegisterView
@@ -6,4 +8,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("register", RegisterView.as_view(), name='register'),
+    path('login', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
