ff97f27bd845aa3f0df4a0c8e5fa9c4eed920b14
Eugene <evgkisevgkis@gmail.com>
2025-12-05 19:39:47 +0500

    создание интерфейса редактирования профиля пользователя

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import HiddenInput
 from users.models import User
 
 
@@ -7,3 +8,12 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ('email', 'password1', 'password2')
         # , 'firstname', 'lastname', 'phone', 'avatar', 'country'
+
+
+class UserProfileForm(UserChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = HiddenInput()
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 from users.apps import UsersConfig
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView, ProfileView
 
 app_name = UsersConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("register", RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
+    path('profile', ProfileView.as_view(), name='profile'),
 ]
