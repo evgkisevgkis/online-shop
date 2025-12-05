73731917ea942c896f5a8d5c56222354bdece3ef
Eugene <evgkisevgkis@gmail.com>
2025-12-05 21:02:34 +0500

    создание механизма запроса нового пароля

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 from users.apps import UsersConfig
 from django.urls import path
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, generate_new_password
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('profile', ProfileView.as_view(), name='profile'),
+    path('genpassword', generate_new_password, name='generate_new_password')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
+from string import ascii_letters,digits
+from random import choices
 
 
 class RegisterView(CreateView):
@@ -18,4 +21,19 @@ class ProfileView(UpdateView):
     success_url = reverse_lazy('users:profile')
     def get_object(self, queryset=None):
         return self.request.user
+
+
+def generate_new_password(request):
+    symbols = ascii_letters + digits
+    randoms = choices(symbols, k=8)
+    new_password = ''.join(randoms)
+    subject = 'Смена пароля'
+    message = f'Вы запросили новый пароль. Вот он: {new_password}'
+    from_email = 'evgeny-kiselev-95@yandex.ru'
+    recipient_list = [request.user.email]
+    send_mail(subject, message, from_email, recipient_list)
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('users:profile'))
+
 
