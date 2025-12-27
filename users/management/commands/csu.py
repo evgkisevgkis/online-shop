4ea5bd69527c62deee1b99034b7d496ddfeac586
Eugene <evgkisevgkis@gmail.com>
2025-12-27 13:03:06 +0500

    редактирование команды csu

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -7,11 +7,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='admin@sky.pro',
-            first_name='Admin',
-            last_name='SkyPro',
-            is_staff=True,
-            is_superuser=True
+        email='admin@sky.pro',
+        first_name='Admin',
+        last_name='SkyPro',
+        is_staff=True,
+        is_superuser=True,
+        code='smth'
         )
 
         user.set_password('123qwe456rty')
