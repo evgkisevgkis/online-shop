f6ae8ff72587bf3362e1a237903dfe7ea9f3442d
Eugene <evgkisevgkis@gmail.com>
2026-07-15 21:14:49 +0500

    установка redis в проект

--- config/settings.py
+++ config/settings.py
@@ -75,6 +75,14 @@ TEMPLATES = [
     },
 ]
 
+CASHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "TIMEOUT": 600
+    }
+}
+
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
